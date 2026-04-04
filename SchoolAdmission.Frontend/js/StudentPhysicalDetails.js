4e2218fce978c34aaca3a43f237864fe67a31666
KunalP1117 <kunalpagare1117@gmail.com>
2026-04-04 12:50:03 +0530

    add a studentphysicaldetails file

---
+++ SchoolAdmission.Frontend/js/StudentPhysicalDetails.js
@@ -0,0 +1,57 @@
+$('#btnSaveStudentInfo').click(function ()
+{
+        var apiBase = "https://localhost:44328/api";
+        const studentId = localStorage.getItem("userId");
+        const payload = {
+           
+            
+             Height:"",
+             Weight:"",
+             HandicappedTypeID:"",
+        };
+      
+        const url = `${apiBase}/student/${studentId}`;
+
+        $('#btnSaveStudentInfo').prop("disabled", true);
+
+
+
+        $.ajax({
+            url: url,
+            method: "PUT",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("accessToken")
+            },
+            contentType: "application/json",
+            data: JSON.stringify(payload),
+
+            success: function (res) {
+                showToast(res.message || "Student information saved successfully", "success");
+            },
+
+            error: function (xhr) {
+                if (xhr.status === 401) {
+                    localStorage.clear();
+                    window.location.href = "../index.html";
+                    return;
+                }
+
+                if (xhr.status === 409 && xhr.responseJSON?.message) {
+                    showToast(xhr.responseJSON.message, "exists");
+                    return;
+                }
+
+                if (xhr.responseJSON?.errors) {
+                    const message = xhr.responseJSON.errors.join("\n");
+                    showToast(message, "error");
+                    return;
+                }
+
+                if (xhr.responseJSON?.message) {
+                    showToast(xhr.responseJSON.message, "error");
+                    return;
+                }
+                showToast("Something went wrong", "error");
+            }
+        });
+    });
