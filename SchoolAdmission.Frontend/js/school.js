f6df2d3acf7f4eec12d3ecb27ee4c2284e2cd83b
BhushanKK <bhushankachave9900@gmail.com>
2026-05-11 20:16:06 +0530

    correct spell

--- SchoolAdmission.Frontend/js/school.js
+++ SchoolAdmission.Frontend/js/school.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    oadCommittees();
+    loadCommittees();
     const table = $('#schoolTable').DataTable({
         ajax: {
             url: `${schoolApi}/AllSchools`,
