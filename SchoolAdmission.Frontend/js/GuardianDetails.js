da4db3967611e79cc50815d67124e877437cfae3
BhushanKK <bhushankachave9900@gmail.com>
2026-05-15 12:51:30 +0530

    revert base url change

--- SchoolAdmission.Frontend/js/GuardianDetails.js
+++ SchoolAdmission.Frontend/js/GuardianDetails.js
@@ -1,10 +1,6 @@
 //Step 4: Parent / Guardian Info
 $(document).ready(function () {
-
-    const token = localStorage.getItem("accessToken");
     const studentId = localStorage.getItem("studentId");
-
-    const headers = getTokenHeader();
 
     $(document).on("click", "#btnSaveParentInfo", function (e) {
 
@@ -59,7 +55,7 @@ $(document).ready(function () {
         $.ajax({
             url: guardianApi,
             method: "POST",
-            headers: headers,
+            headers: getTokenHeader(),
             contentType: "application/json",
             data: JSON.stringify(payload),
 
